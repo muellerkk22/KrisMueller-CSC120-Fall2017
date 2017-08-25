942f1eb9380df218ad1abcfe0fe271cf05600161
Kris Mueller <muellerkk22@gmail.com>
2017-08-25 02:08:50 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,12 @@
+Select 3 numbers randomly 
+    seed random
+    The highest and lowest numbers are the ranges for the 3rd number
+initiate countdown variable to ten 
+    (used when random number not guessed)
+tell user to select between the 2 numbers (highest random number and lowest random number) randomly generated
+    read from stdin
+if number is guessed correctly
+    tell user
+Otherwise, tell user whether random number is higher or lower than guess
+    and subtract 1 from countdown variable if number not guessed
+
