2f24e80167512f8af8a9c6bbe5892a95e32e3313
Kris Mueller <muellerkk22@gmail.com>
2017-08-30 01:21:19 +0000

    Lab1.2 Commit

---
+++ lab12/lab1.2.cpp
@@ -0,0 +1,16 @@
+ 
+ #include <iostream>    //preprocessor
+ using namespace std;   //directives
+ 
+ int main() //beginning of main function
+ {
+cout <<"     '0'       "<< endl;
+cout <<"    .0.'.      "<< endl;
+cout <<"   .'.'0'.     "<< endl;
+cout <<"  0'.0.'.0.    "<< endl;
+cout <<" .'.0.'.'.0.   "<< endl;
+cout <<".0.0.'.0.'.0.  "<< endl;
+cout <<"     | |       "<< endl;
+
+ }
+
---
+++ lab12/lecture1.2.cpp
@@ -0,0 +1,15 @@
+
+ #include <iostream>    //preprocessor
+ using namespace std;   //directives
+ 
+ int main() //beginning of main function
+ {
+     cout<<"Hello "<<"World!"<<endl; //body of main function
+ }
+ 
+ //Identify the following parts of the program:
+ //   preprocessor directives
+ //   main function
+ //   comments- were at the top of file
+ //update it so that it is human readable
+ //add a second output line that says "Hello YourName!"
