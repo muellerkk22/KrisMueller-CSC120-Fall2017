2577bfc5c7baafef3ba800b244805fb369dfbc1f
Kris Mueller <muellerkk22@gmail.com>
2017-09-20 01:30:24 +0000

    Lab2.1 submission

--- lab21/lab21.cpp
+++ lab21/lab21.cpp
@@ -27,17 +27,16 @@ cout << "Please enter the wind velocity" << endl;
 cin >> wind_velocity;
 
 //calculate old style wind chill
-//windchill = 0.081 * (3.71*square root(velocity) + 5.81 - 0.25 * velocity) * (temp in F - 91.4) + 91.4
 old_wind_chill = 0.081 * (3.71 * sqrt(wind_velocity) + 5.81 - 0.25 * wind_velocity) * (temperature - 91.4) + 91.4;
 
 
 //calculate new style wind chill
-//windchill = 35.74 + .6215 * temp in F - 35.75 * velocity^0.16 + .4275 * temp in F * velocity ^ 0.16
 new_wind_chill = 35.74 + .6215 * temperature - 35.75 * pow (wind_velocity, 0.16) + .4275 * temperature * pow (wind_velocity, 0.16);
 
 
+//Print out results
 cout << "Wind Velocity\t" << "Old Style Wind Chill\t" << "New Style Wind Chill\t" << "Difference" << endl;
-cout << wind_velocity << "\t\t" << old_wind_chill << "\t\t" << new_wind_chill << "\t\t" << abs(old_wind_chill - new_wind_chill) << endl;
+cout << wind_velocity << "\t\t" << old_wind_chill << "\t\t\t" << new_wind_chill << "\t\t\t" << abs(old_wind_chill - new_wind_chill) << endl;
     return 0;
     
 }
